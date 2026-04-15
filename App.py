acd547779695ef9ae73da4c2cc6fca97eda480a8
Abhishek H N <1NC25MC001@ncetmail.com>
2026-04-15 14:49:42 +0530

    Update App.py

--- App.py
+++ App.py
@@ -2,7 +2,7 @@ import streamlit as st
 from transformers import pipeline
 @st.cache_resource
 def load_summarizer():
-  return pipeline("summarization", model="import streamlit as st
+  return pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
 from transformers import pipeline
 @st.cache_resource
 def load_summarizer():
